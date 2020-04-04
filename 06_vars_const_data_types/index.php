17071e95c42989d1c616075709e7e9cf5a171aa6
bulatx <bulatxajrutdinoff@yandex.ru>
2020-04-04 11:19:55 +0300

    Ссылки (жесткие, символические)

--- 06_vars_const_data_types/index.php
+++ 06_vars_const_data_types/index.php
@@ -1,15 +1,39 @@
 <!doctype html>
 <html lang="en">
 <head>
-  <meta charset="UTF-8">
-  <meta name="viewport"
-        content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Глава 6. Переменные, константы, типы данных</title>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Глава 6. Переменные, константы, типы данных</title>
 </head>
 <body>
 
 <?php
+
+/* Жеские ссылки */
+$a = 10;
+$b =& $a;
+$b = 11;
+echo $a;
+br();
+
+$a = [
+  'a' => 'value a',
+  'b' => 'value b',
+];
+
+$b =& $a['c'];
+$b = 'c';
+
+
+/* Символическая ссылка */
+$a = 'var $a';
+$b = 'a';
+echo $$b;
+br();
+
+hr();
 
 $rand = mt_rand(1, 10);
 $dynamic_const = "CONST_{$rand}";
@@ -24,16 +48,26 @@ class SomeClass
 }
 
 $a = [
-    1,
-    [
-        "Programs hacking programs. Why?",
-        "d'Artaniyan"
-    ],
+  1,
+  [
+    "Programs hacking programs. Why?",
+    "d'Artaniyan"
+  ],
 ];
 
 echo "<pre>" . var_export($a, true) . "</pre>";
 $obj1 = new SomeClass();
 echo "<pre>" . var_export($obj1, true) . "</pre>";
+
+function br()
+{
+  echo "<br>";
+}
+
+function hr()
+{
+  echo "<hr>";
+}
 
 ?>
 
