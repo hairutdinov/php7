59c84591a292d9f267469054250b2bf66b20be8b
bulatx <bulatxajrutdinoff@yandex.ru>
2020-04-04 17:05:24 +0300

    Файл с функциями [v.2]

--- functions.php
+++ functions.php
@@ -1,10 +1,17 @@
 <?php
-function br()
+function br($return = false)
 {
+  if ($return) return "<br>";
   echo "<br>";
 }
 
-function hr()
+function hr($return = false)
 {
+  if ($return) return "<hr>";
   echo "<hr>";
+}
+
+
+function h2 ($str) {
+  echo "<h2>{$str}</h2>";
 }
