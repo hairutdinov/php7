1ac73e0c912d501668a926a88b244c8c4f34bc4c
bulatx <bulatxajrutdinoff@yandex.ru>
2020-04-29 09:47:24 +0300

    Глава 32. Сетевые функции

---
+++ 32_network_functions/index.php
@@ -0,0 +1,31 @@
+<?php  //declare(strict_types = 1); ?>
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Глава 32. Сетевые функции</title>
+</head>
+<body>
+
+<?php require_once "../functions.php" ?>
+
+<?php
+var_dump(gethostbyaddr('87.250.250.242'));
+
+br();
+
+var_dump(gethostbyname('ya.ru'));
+
+br();
+
+var_dump(gethostbynamel('ya.ru'));
+
+
+
+?>
+
+</body>
+</html>
